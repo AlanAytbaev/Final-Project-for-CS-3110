f4f8ececd686c45a1ea66be9a446580d08294323
Saranya <ss2625@cornell.edu>
2019-11-02 13:51:49 -0400

    integration of main and arithmetic

--- arithmetic.ml
+++ arithmetic.ml
@@ -1,91 +1,79 @@
-(** A module that implements the functions needed for the arithmetic cfu. A modu    le that matches [Arithmetic_Funcs] is suitable for use in [Arithmetic_CFU]. *)
+(** A module that implements the functions needed for the arithmetic cfu. 
+    A module that matches [Arithmetic_Funcs] is suitable for use in 
+    [Arithmetic_CFU]. *)
 module type Arithmetic_Funcs = sig
-
-        (** [add s] returns the result of adding the first element of s to the
-            second element of s. *)
-        val add : float list -> float
-
-        (** [subtract s] returns the result of subtracting the first element of             s from the second element of s. *)
-        val subtract : float list -> float
-
-        (** [multiply s] returns the result of multiplying the first element of             s by the second element of s. *)
-        val multiply : float list -> float
-
-        (** [divide s] returns the result of dividing the first element of s by             the second element of s. *)
-        val divide : float list -> float
-
-        val exponentiation : float list -> float
-
-        val modulus : float list -> float
-
-        val logarithm : float list -> float
+  val add2 : float list -> float
+  val subtract : float list -> float
+  val multiply : float list -> float
+  val divide : float list -> float
+  val exponentiation : float list -> float
+  val modulus : float list -> float
+  val logarithm : float list -> float
 end
 
 module type CFU_sig = sig
-
-        (** type primitive is the type of the value that the calculator works wi            th*)
-        type primitive
-
-        (** An [operation_list] is an association list that maps operation symbo            ls to functions *)
-        val operation_list : (string * ( primitive  list -> primitive )) list
+  (** type primitive is the type of the value that the calculator works with*)
+  type primitive
+  (** An [operation_list] is an association list that maps operation symbols 
+      to functions *)
+  val operation_list : (string * ( primitive  list -> primitive )) list
 end
 
 module Arithmetic_Functions : Arithmetic_Funcs = struct
 
-        let add (s : float list) =
-                match s with
-                | hd1::hd2::tl -> (Float.add hd1 hd2)
-                | _ -> failwith "InvalidInput"
+  let add2 (s : float list) =
+    match s with
+    | hd1::hd2::tl -> (Float.add hd1 hd2)
+    | _ -> failwith "InvalidInput"
 
+  let subtract (s : float list) =
+    match s with
+    | hd1::hd2::tl -> (Float.sub hd1 hd2)
+    | _ -> failwith "InvalidInput"
 
-        let subtract (s : float list) =
-                match s with
-                | hd1::hd2::tl -> (Float.sub hd1 hd2)
-                | _ -> failwith "InvalidInput"
+  let multiply (s : float list) =
+    match s with
+    | hd1::hd2::tl -> (Float.mul hd1 hd2)
+    | _ -> failwith "InvalidInput"
 
-        let multiply (s : float list) =
-                match s with
-                | hd1::hd2::tl -> (Float.mul hd1 hd2)
-                | _ -> failwith "InvalidInput"
+  let divide (s : float list) =
+    match s with
+    | hd1::hd2::tl -> (Float.div hd1 hd2)
+    | _ -> failwith "InvalidInput"
 
-        let divide (s : float list) =
-                match s with
-                | hd1::hd2::tl -> (Float.div hd1 hd2)
-                | _ -> failwith "InvalidInput"
+  let exponentiation (s : float list) =
+    match s with
+    | hd1::hd2::tl -> (hd1 ** hd2)
+    | _ -> failwith "InvalidInput"
 
-        let exponentiation (s : float list) =
-                match s with
-                | hd1::hd2::tl -> (hd1 ** hd2)
-                | _ -> failwith "InvalidInput"
+  let modulus (s : float list) =
+    match s with
+    | hd1::hd2::tl ->  (Float.sub hd1 (Float.mul hd2 (Float.div hd1 hd2)))
+    | _ -> failwith "InvalidInput"
 
-        let modulus (s : float list) =
-                match s with
-                | hd1::hd2::tl ->  (Float.sub hd1 (Float.mul hd2 (Float.div hd1 hd2)))
-                | _ -> failwith "InvalidInput"
+  let rec log (a : float) (b : float) =
+    let n = (Float.compare a b) in
+    match n with
+    | _ when n < 0 -> 0.0
+    | _ -> 1.0 +. log (Float.div a b) b
 
-        let rec log (a : float) (b : float) =
-                let n = (Float.compare a b) in
-                match n with
-                | _ when n < 0 -> 0.0
-                | _ -> 1.0 +. log (Float.div a b) b
-
-        let logarithm (s : float list) =
-                match s with
-                | hd1::hd2::tl -> log hd1 hd2
-                | _ -> failwith "InvalidInput"
+  let logarithm (s : float list) =
+    match s with
+    | hd1::hd2::tl -> log hd1 hd2
+    | _ -> failwith "InvalidInput"
 end
 
 module Arithmetic_CFU : CFU_sig = struct
 
-        type primitive = float
+  type primitive = float
 
-        let operation_list = [
-                ("+", Arithmetic_Functions.add);
-                ("-", Arithmetic_Functions.subtract);
-                ("*", Arithmetic_Functions.multiply);
-                ("/", Arithmetic_Functions.divide);
-                ("^", Arithmetic_Functions.exponentiation);
-                ("%", Arithmetic_Functions.modulus);
-                ("log", Arithmetic_Functions.logarithm)
-        ];
+  let operation_list = [
+    ("+", Arithmetic_Functions.add2);
+    ("-", Arithmetic_Functions.subtract);
+    ("*", Arithmetic_Functions.multiply);
+    ("/", Arithmetic_Functions.divide);
+    ("^", Arithmetic_Functions.exponentiation);
+    ("%", Arithmetic_Functions.modulus);
+    ("log", Arithmetic_Functions.logarithm)
+  ]
 end
