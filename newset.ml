7814e8b68cccc7d0b01d788e8958ccdfdbfd5f31
Emily Weed <emw232@cornell.edu>
2019-12-08 23:36:39 -0500

    documentation2

--- newset.ml
+++ newset.ml
@@ -1,9 +1,8 @@
 open Ast
 
-(** An abstract module type that is meant to structure the CFU modules 
-    (ie. Aritmetic functions, calculus functions, statistics functions).
-     A module that matches [CFU_sig] is suitable for use in the 
-     [Calc] module. *)
+(** An abstract module type that is meant to structure the CFU modules (ie. Arit
+    metic functions, calculus functions, statistics functions). A module that ma 
+    tches [CFU_sig] is suitable for use in the [Calc] module. *)
 
 module type CFU_sig = sig
   val operation_list : (string * ( value list -> value)) list
@@ -29,9 +28,13 @@ module NewSet_Functions : NewSet_Funcs = struct
     | VRow x -> x
     | _ -> failwith "This cannot occur - matrix.ml"
 
+  let helper' lst = 
+    Array.of_list lst 
 
-  (** [check x y int bool] checks whether every element in x is present in y and
-      returns [bool] true if so and false if otherwise *)
+  (** [check_elements_helper x y int bool] checks whether every element in x is present in y and
+       returns [bool] true if so and false if otherwise *)
+
+
   let rec check_elements_helper x y int bool= 
     match int with 
     |0 -> bool
@@ -42,6 +45,7 @@ module NewSet_Functions : NewSet_Funcs = struct
   (** [isEqual_helper x y] is true if two sets contain structurally 
       equal elements and have the same number of elements and 
       is false otherwise*)
+
   let isEqual_helper x y =
     if (Array.length x = Array.length y) then 
       check_elements_helper x y (Array.length x) true
@@ -52,22 +56,21 @@ module NewSet_Functions : NewSet_Funcs = struct
     let s2 = List.nth v 1 |> unwrap_row in 
     VBool (isEqual_helper s1 s2)
 
-  (** [isSubset x y] returns true if all elements in [x] are also in [y] *)
+  (** [isSubset x y] returns true if all elements in [x] is also there in [y] *)
   let isSubset v = 
     let s1 = List.nth v 0 |> unwrap_row in 
     let s2 = List.nth v 1 |> unwrap_row in 
     VBool(check_elements_helper s1 s2 (Array.length s1) true)
 
-
-  (** [check_elements_helper_2 x y i b ] is true iff *)
   let rec check_elements_helper_2 x y int bool= 
     match int with 
     |0 -> bool
     |i -> if (Array.mem x.(i-1) y) 
       then  false
-      else check_elements_helper x y (i-1) (true&&bool) 
+      else check_elements_helper_2 x y (i-1) (true&&bool) 
 
-  (** NOT CORRECT RIGHT NOW *)
+  (** [isDisjoint x y] returns true if none of the elements in [x] are
+      in [y] and returns false otherwise*)
   let isDisjoint v = 
     let s1 = List.nth v 0 |> unwrap_row in 
     let s2 = List.nth v 1 |> unwrap_row in 
